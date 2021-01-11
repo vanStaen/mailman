6ee15dd65c2a9817bd6aa2b13069d96df6a311f6
Clément van Staen <clement.vanstaen@gmail.com>
2021-01-11 11:08:37 +0100

    function body

--- api.php
+++ api.php
@@ -3,22 +3,13 @@
 header("Content-Type:application/json");
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
-$entityBody = file_get_contents('php://input');
+//$entityBody = file_get_contents('php://input');
+$entityBody = detectRequestBody();
 
 if(!empty($_GET['key']))
 {
 	$key=$_GET['key'];
-	
-	/* if(empty($price))
-	{
-		response(200,"Product Not Found",NULL);
-	}
-	else
-	{
-		response(200,"Product Found",$price);
-	} */
-
-	response(200, $key." ".$httpMethod ,$entityBody);
+	response(200, $key ,$entityBody);
 	
 }
 else
@@ -26,14 +17,25 @@ else
 	response(400,"Invalid Request",NULL);
 }
 
+
+
+
 function response($status,$status_message,$data)
 {
-	header("HTTP/1.1 ".$status);
-	
+	header("HTTP/1.1 ".$status);	
 	$response['status']=$status;
 	$response['status_message']=$status_message;
-	$response['data']=$data;
-	
+	$response['data']=$data;	
 	$json_response = json_encode($response);
 	echo $json_response;
+}
+
+
+function detectRequestBody() {
+    $rawInput = fopen('php://input', 'r');
+    $tempStream = fopen('php://temp', 'r+');
+    stream_copy_to_stream($rawInput, $tempStream);
+    rewind($tempStream);
+
+    return $tempStream;
 }
